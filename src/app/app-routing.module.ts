f4e84f0ff229f64b862de7f1f372b12e5125bbed
pasinduJDJ <pasindudulanjanarj@gmail.com>
2024-06-21 09:07:45 +0530

    Header Complete

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'app', pathMatch: 'full'},
+  {path: 'app', component:HomeComponent, children:[
+    {path: '', redirectTo:'home',pathMatch: 'full'},
+    {path: 'home', component: HomeComponent},
+  ]},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
