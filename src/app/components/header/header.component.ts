a2265abeaeccf25e97382b242d0f5e47547aaec2
pasinduJDJ <pasindudulanjanarj@gmail.com>
2024-06-21 23:20:22 +0530

    finish Login Page

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { SignInPageComponent } from '../sign-in-page/sign-in-page.component';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
-
+export class HeaderComponent implements OnInit {
+  
+  constructor(
+    private ngbModal:NgbModal,
+  ){}
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+  openLogin(){
+    this.ngbModal.open(SignInPageComponent)
+ }
 }
